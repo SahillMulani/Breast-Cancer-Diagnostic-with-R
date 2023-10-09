a6a8b91d3294f561072575c7930d9aef0c3c3410
SahillMulani <smulani239@gmail.com>
2023-10-09 14:11:09 +0100

    Update Breast Cancer.R

--- Breast Cancer.R
+++ Breast Cancer.R
@@ -1,4 +1,19 @@
 # Open Csv File
 wdbc<-read.csv(file="data.csv", stringsAsFactors = FALSE)
-
+# Get the Structure of the Data
 str(wdbc)
+
+#First variable "id" is just a unique identifier of Patient
+# It does not contribute anything to the data therefore we will remove it
+
+#Droping the id column
+wdbc <- wdbc[-1]
+
+#Diagonistic Column 
+table(wdbc$diagnosis)
+
+# Giving Diagnosis Variable informative labels
+wdbc$diagnosis <- factor(wdbc$diagnosis, levels = c("B","M"), 
+                         labels = c("Benign", "Malignant"))
+
+round(prop.table(table(wdbc$diagnosis)) * 100, digits = 1)
